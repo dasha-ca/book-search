0e6c77a17175786a66736914691e0aacd67af095
dasha-ca <daria.romanova121@gmail.com>
2022-01-25 10:33:22 -0500

    updated to https

--- src/services/BooksService.ts
+++ src/services/BooksService.ts
@@ -4,17 +4,15 @@ import { SearchResultsIsbn } from "../interfaces/SearchResultsIsbn";
 class BooksDataService {
     public async getBooksByTitle(bookTitle: string) {
         bookTitle = encodeURIComponent(bookTitle);
-        const result = await fetch(`http://openlibrary.org/search.json?q=${bookTitle}&limit=10`);
+        const result = await fetch(`https://openlibrary.org/search.json?q=${bookTitle}&limit=10`);
         const searchResult = await result.json() as BookSearchResult;
-        console.log("getBooksByTitle results", searchResult);
         return searchResult.docs;
     }
 
     public async getBooksByIsbn(bookIsbn: string) {
         bookIsbn = encodeURIComponent(bookIsbn);
-        const result = await fetch(`http://openlibrary.org/api/volumes/brief/isbn/${bookIsbn}.json`);
+        const result = await fetch(`https://openlibrary.org/api/volumes/brief/isbn/${bookIsbn}.json`);
         const searchResult = await result.json() as SearchResultsIsbn;
-        console.log("getBooksByIsbn results", searchResult);
         return searchResult.items;
     }
 }
